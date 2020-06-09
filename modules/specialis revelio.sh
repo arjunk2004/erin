7f7dd36e1bf0ed4548257bde5a0b1fd81d70372a
Arjun Khanna <arjunk2022@headroyce.org>
2020-06-09 16:03:13 -0700

    Specialis revelio easter egg

---
+++ modules/specialis revelio.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+zenity --text="This program is the property of the Quarter Bone Peasant" --width=500 --info
--- start.sh
+++ start.sh
@@ -2,13 +2,17 @@
 IFS=
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-input=`zenity --text="How can I assist you?" --entry`
+input=`zenity --text="How can I assist you?" --width=500 --entry`
 if [ $input" " = " " ]
 then exit 1
 fi
 IFS=" " read cmd argument <<< $input
-unset IFS
-cmd=$DIR"/modules/"$cmd".sh \"$argument\""
-fallback=$DIR"/modules/google.sh \"$input\""
-echo $cmd
+if [ $input = "specialis revelio" ]
+then
+	cmd=$DIR"/modules/specialis revelio.sh"
+else
+	unset IFS
+	cmd=$DIR"/modules/"$cmd".sh \"$argument\""
+	fallback=$DIR"/modules/google.sh \"$input\""
+fi
 $cmd || $fallback
